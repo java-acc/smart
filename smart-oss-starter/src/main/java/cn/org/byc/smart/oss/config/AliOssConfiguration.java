4ec449ae4d5250f8cc9ec97bd9b40c379cf4b8de
Ken <kan.zhang-cn@hotmail.com>
2025-02-20 19:17:27 +0800

    feat(oss): configure AliOss with advanced client settings and conditional beans
    Enhances AliOssConfiguration with:
    - Conditional configuration for Aliyun OSS
    - Configurable OSS client with optimized connection settings
    - Flexible OSS rule beans for default and tenant modes
    - Automatic AliOssTemplate creation with OSS client and rule

--- smart-oss-starter/src/main/java/cn/org/byc/smart/oss/config/AliOssConfiguration.java
+++ smart-oss-starter/src/main/java/cn/org/byc/smart/oss/config/AliOssConfiguration.java
@@ -16,8 +16,66 @@
 
 package cn.org.byc.smart.oss.config;
 
+import cn.org.byc.smart.oss.props.OssProperties;
+import cn.org.byc.smart.oss.rule.DefaultOssRule;
+import cn.org.byc.smart.oss.rule.OssRule;
+import cn.org.byc.smart.oss.rule.TenantOssRule;
+import cn.org.byc.smart.oss.template.AliOssTemplate;
+import com.aliyun.oss.ClientConfiguration;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.common.auth.CredentialsProvider;
+import com.aliyun.oss.common.auth.DefaultCredentialProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
+@EnableConfigurationProperties(OssProperties.class)
+@ConditionalOnProperty(value = "smart.oss.name", havingValue = "ali")
 public class AliOssConfiguration {
+    private OssProperties ossProperties;
+
+    @Bean
+    @ConditionalOnMissingBean(OssRule.class)
+    @ConditionalOnProperty(value = "smart.oss.tenantMode", havingValue = "false", matchIfMissing = true)
+    public OssRule ossRule(){
+        return new DefaultOssRule();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(OssRule.class)
+    @ConditionalOnProperty(value = "smart.oss.tenantMode", havingValue = "true")
+    public OssRule tenantOssRule(){
+        return new TenantOssRule();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(OSSClient.class)
+    public OSSClient ossClient(){
+        ClientConfiguration configuration = new ClientConfiguration();
+        // 设置OSSClient允许打开的最大Http连接数
+        configuration.setMaxConnections(ClientConfiguration.DEFAULT_MAX_CONNECTIONS);
+        // 设置Socket层传输数据的超时时间
+        configuration.setSocketTimeout(ClientConfiguration.DEFAULT_SOCKET_TIMEOUT);
+        // 设置建立连接的超时时间
+        configuration.setConnectionTimeout(ClientConfiguration.DEFAULT_CONNECTION_TIMEOUT);
+        // 设置失败请求重试次数(默认3次)
+        configuration.setMaxErrorRetry(ClientConfiguration.DEFAULT_MAX_RETRIES+2);
+        // 设置连接空闲的超时时间，超时则关闭连接
+        configuration.setIdleConnectionTime(ClientConfiguration.DEFAULT_IDLE_CONNECTION_TIME);
+        // 设置从连接池中获取连接的超时时间
+        configuration.setConnectionRequestTimeout(ClientConfiguration.DEFAULT_CONNECTION_REQUEST_TIMEOUT);
+        CredentialsProvider credentialsProvider = new DefaultCredentialProvider(ossProperties.getAccessKey(), ossProperties.getSecretKey());
+        return new OSSClient(ossProperties.getEndpoint(),credentialsProvider,configuration);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(AliOssTemplate.class)
+    @ConditionalOnBean({OSSClient.class, OssRule.class})
+    public AliOssTemplate aliOssTemplate(OSSClient ossClient, OssRule ossRule){
+        return new AliOssTemplate(ossClient, ossProperties, ossRule);
+    }
 }
